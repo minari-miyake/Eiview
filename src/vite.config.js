41db7532dabfad910b9269a169e7677b8c17a453
yuzuki-yoshizumi <yuzuki_yoshizumi@NCT2021-08.com-net.comthink.co.jp>
2025-06-12 18:17:15 +0900

    ローカル変更を一時的にコミット

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+    host: '0.0.0.0', // ← ここが重要
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+  },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
