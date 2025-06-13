8fb31a1f72313d2e008e820d634388c4e1170515
itsuki-yura <itsuki-yura@comthink.co.jp>
2025-06-13 15:47:43 +0900

    ローカルのvite.config.jsをコミット

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+ 
 export default defineConfig({
-    server: {
-    host: '0.0.0.0', // ← ここが重要
-    port: 5173,
-    strictPort: true,
-    watch: {
-      usePolling: true,
-    },
-  },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
@@ -23,3 +18,5 @@ export default defineConfig({
         }
     }
 });
+ 
+ 
