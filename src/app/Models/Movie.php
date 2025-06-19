fa94d4fb23a2c89f7d20e5c8ca5b750ed772f50f
itsuki-yura <itsuki-yura@comthink.co.jp>
2025-06-19 11:05:37 +0900

    feat: Force merge Movie.php from 07_login branch

--- src/app/Models/Movie.php
+++ src/app/Models/Movie.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'release_date',
+        'genre',
+        'director',
+        'poster_url',
+        'rating',
+        'duration',
+    ];
+
+    protected $casts = [
+        'release_date' => 'date',
+        'rating' => 'decimal:1',
+    ];
 }
