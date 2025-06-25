dfcb209dd4281e85089f8b2d8816c6ee2dc10572
itsuki-yura <itsuki-yura@comthink.co.jp>
2025-06-25 17:23:42 +0900

    レイアウト変更したよ

--- src/app/Http/Controllers/User/ReviewHistoryController.php
+++ src/app/Http/Controllers/User/ReviewHistoryController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ReviewHistoryController extends Controller
 {
     public function index()
     {
-        $reviews = Auth::user()->reviews()->with('movie')->latest()->paginate(10);
+        try {
+            $reviews = Auth::user()->reviews()->with('movie')->latest()->paginate(10);
+        } catch (\Exception $e) {
+            $reviews = new LengthAwarePaginator(
+                collect([]),
+                0,
+                10,
+                1,
+                ['path' => request()->url(), 'pageName' => 'page']
+            );
+        }
         return view('reviews.history', compact('reviews'));
     }
 }
